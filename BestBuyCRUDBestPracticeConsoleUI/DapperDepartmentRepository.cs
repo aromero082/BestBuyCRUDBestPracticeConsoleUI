aeae47b264899b98ed081b995ac9eaaae630b3c8
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2022-06-08 15:17:38 -0700

    DapperExercise

---
+++ BestBuyCRUDBestPracticeConsoleUI/DapperDepartmentRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Text;
+
+namespace BestBuyCRUDBestPracticeConsoleUI
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+        //Constructor
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            //Constructor
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            var depos = _connection.Query<Department>("Select * From departments");
+
+            return depos;
+        }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+            new { departmentName = newDepartmentName });
+
+        }
+    }
+}
--- BestBuyCRUDBestPracticeConsoleUI/Program.cs
+++ BestBuyCRUDBestPracticeConsoleUI/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 
 namespace BestBuyCRUDBestPracticeConsoleUI
 {
@@ -6,7 +11,46 @@ namespace BestBuyCRUDBestPracticeConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Configuration
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            #endregion
+            IDbConnection conn = new MySqlConnection(connString);
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Hello user, here are the current departments:");
+            Console.WriteLine("Plese press enter...");
+            Console.ReadLine();
+
+            var depos = repo.GetAllDepartments();
+            print(depos);
+
+
+            Console.WriteLine("Do you want to add a department???");
+            string userResponse = Console.ReadLine();
+
+            if (userResponse.ToLower() == "Yes")
+            {
+                Console.WriteLine("What is the nname of your new Department???");
+                userResponse = Console.ReadLine();
+
+                repo.InsertDepartment(userResponse);
+                print(repo.GetAllDepartments());
+            }
+
+            Console.WriteLine("Have a great day");
+
+        }
+        private static void print(IEnumerable<Department> depos)
+        {
+            foreach(var depo in depos)
+            {
+                Console.WriteLine($"ID: {depo.DepartmentID} Name: {depo.Name}");
+            }
         }
     }
 }
